33660306cf987027b971bcdb08c6c037bc619776
bartoszolszewski6 gmail.com <bartoszolszewski6@gmail.com>
2020-09-19 11:42:00 +0200

    Dodaj pliki projektów.

---
+++ Pentago/Board.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Pentago
+{
+    class Board
+    {
+        private Button[][] squaresButton;
+        private Button[] allSquares;
+        static int SizeOfSquare = 9;
+        public Board(Button[] squareButton1, Button[] squareButton2, Button[] squareButton3, Button[] squareButton4)
+        {
+
+            squaresButton = new Button[4][];
+            squaresButton[0] = squareButton1;
+            squaresButton[1] = squareButton2;
+            squaresButton[2] = squareButton3;
+            squaresButton[3] = squareButton4;
+            allSquares = new Button[SizeOfSquare * 4];
+            for (int k = 0; k < 2; k++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        allSquares[i + (j * 6) + (18 * k)] = squaresButton[2*k][i + (j * 3)];
+                    }
+                    for (int i = 0; i < 3; i++)
+                    {
+                        allSquares[i + 3 + (j * 6) + (18 * k)] = squaresButton[2*k+1][i + (j * 3)];
+
+                    }
+                }
+            }
+        }
+        public void IsEnabledFalse()
+        {
+
+            foreach(Button[] squares in squaresButton)
+            {
+                foreach(Button ball in squares)
+                {
+                    ball.IsEnabled = false;
+                }
+            }
+        }
+        public void IsEnabledTrue()
+        {
+
+            foreach (Button[] squares in squaresButton)
+            {
+                foreach (Button ball in squares)
+                {
+                    ball.IsEnabled = true;
+                }
+            }
+        }
+
+        public void Rotation(char direction, int square)
+        {
+            Brush[] copySquare = new Brush[SizeOfSquare];
+            for (int i = 0; i < SizeOfSquare; i++)
+            {
+                copySquare[i] = squaresButton[square][i].Background;
+            }
+            
+            int index = 0;
+            if(direction == 'N') //'N' is oposit direction to clock move
+            {            
+                for(int j=0; j<3; j++)
+            { 
+                for(int i=6; i>=0; i-=3)
+                {
+                squaresButton[square][i+j].Background = copySquare[index++];
+                }
+            }
+
+            }
+            else { //direction like clock move
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int i = 6; i >= 0; i -= 3)
+                    {
+                        squaresButton[square][index++].Background = copySquare[j+i];
+                    }
+                }
+            }
+        }
+        public bool WinCheck(Brush color)
+        {
+            int ballsInLine = 0;
+            for (int i = 0; i < 36; i += 6) //move about 6 balls, because we move by all line
+            {
+                for (int j = 0; j < 2; j++) //we can make 5 ball in line whan we will start from first or second ball
+                {
+                    for (int k = 0; k < 5; k++) //if all 5 ball have wanted color
+                    {
+                        if (allSquares[i + j + k].Background == color)
+                        {
+                            ballsInLine++;
+                        }
+                        else break;
+                    }
+                    if (ballsInLine == 5) return true;
+                    else ballsInLine = 0;
+                }
+            }
+                for (int i = 0; i <= 6; i++) //move about 6 balls, because we move by all line
+                {
+                    for (int j = 0; j <= 6; j+=6) //we can make 5 ball in line whan we will start from first or second ball
+                    {
+                        for (int k = 0; k <= 24; k+=6) //if all 5 ball have wanted color
+                        {
+                            if (allSquares[i + j + k].Background == color)
+                            {
+                                ballsInLine++;
+                            }
+                            else break;
+                        }
+                        if (ballsInLine == 5) return true;
+                        else ballsInLine = 0;
+                    }
+                }
+            for(int i=0; i<8; i++)
+            {
+                if (i == 2) i = 6;
+                for(int j=0; j<=28; j+=7)
+                {
+                    if (allSquares[i + j].Background == color)
+                    {
+                        ballsInLine++;
+                    }
+                    else break;
+                }
+                if (ballsInLine == 5) return true;
+                else ballsInLine = 0;
+            }
+            for (int i = 4; i <=11; i++)
+            {
+                if (i == 6) i = 10;
+                for (int j = 0; j <= 24; j += 5)
+                {
+                    if (allSquares[i + j].Background == color)
+                    {
+                        ballsInLine++;
+                    }
+                    else break;
+                }
+                if (ballsInLine == 5) return true;
+                else ballsInLine = 0;
+            }
+            return false;
+        }
+        public bool FullBoard()
+        {
+            foreach(Button ball in allSquares)
+            {
+                if (ball.Background == Brushes.Transparent) return false;
+            }
+            return true;
+        }
+
+        public bool ShowWiner()
+        {
+            bool whiteWin = WinCheck(Brushes.White);
+            bool blackWin = WinCheck(Brushes.Black);
+            if(whiteWin == true && blackWin == true)
+            {
+                MessageBox.Show("Draw!");
+                IsEnabledFalse();
+                return true;
+            }
+            else if(whiteWin)
+            {
+                MessageBox.Show("White won!");
+                IsEnabledFalse();
+                return true;
+            }
+            else if (blackWin)
+            {
+                MessageBox.Show("Black won!");
+                IsEnabledFalse();
+                return true;
+            }
+            else if(FullBoard())
+            {
+                MessageBox.Show("Draw!");
+                IsEnabledFalse();
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Pentago/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Pentago
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            blackMovement = true;
+            squareButon1 = new Button[9];
+            squareButon1[0] = Button11; squareButon1[1] = Button12; squareButon1[2] = Button13; squareButon1[3] = Button14; squareButon1[4] = Button15;
+            squareButon1[5] = Button16; squareButon1[6] = Button17; squareButon1[7] = Button18; squareButon1[8] = Button19;
+            squareButon2 = new Button[9];
+            squareButon2[0] = Button21; squareButon2[1] = Button22; squareButon2[2] = Button23; squareButon2[3] = Button24; squareButon2[4] = Button25;
+            squareButon2[5] = Button26; squareButon2[6] = Button27; squareButon2[7] = Button28; squareButon2[8] = Button29;
+            squareButon3 = new Button[9];
+            squareButon3[0] = Button31; squareButon3[1] = Button32; squareButon3[2] = Button33; squareButon3[3] = Button34; squareButon3[4] = Button35;
+            squareButon3[5] = Button36; squareButon3[6] = Button37; squareButon3[7] = Button38; squareButon3[8] = Button39;
+            squareButon4 = new Button[9];
+            squareButon4[0] = Button41; squareButon4[1] = Button42; squareButon4[2] = Button43; squareButon4[3] = Button44; squareButon4[4] = Button45;
+            squareButon4[5] = Button46; squareButon4[6] = Button47; squareButon4[7] = Button48; squareButon4[8] = Button49;
+            arrowsButton = new Button[8];
+            arrowsButton[0] = ButtonArrow1P; arrowsButton[1] = ButtonArrow1N; arrowsButton[2] = ButtonArrow2P; arrowsButton[3] = ButtonArrow2N;
+            arrowsButton[4] = ButtonArrow3P; arrowsButton[5] = ButtonArrow3N; arrowsButton[6] = ButtonArrow4P; arrowsButton[7] = ButtonArrow4N;
+            board = new Board(squareButon1, squareButon2, squareButon3, squareButon4);
+            foreach (Button arrow in arrowsButton)
+            {
+                arrow.Visibility = Visibility.Hidden;
+            }
+        }
+            private bool blackMovement;
+        private Button[] squareButon1;
+        private Button[] squareButon2;
+        private Button[] squareButon3;
+        private Button[] squareButon4;
+        private Button[] arrowsButton;
+        private Board board;
+        private void ButtonBall_Click(object sender, RoutedEventArgs e)
+        {
+            if(blackMovement) (sender as Button).Background = Brushes.Black;
+            else (sender as Button).Background = Brushes.White;
+            blackMovement = !blackMovement;            
+            board.IsEnabledFalse();
+            if(!board.ShowWiner())
+            { 
+            foreach (Button arrow in arrowsButton)
+            {
+                arrow.Visibility = Visibility.Visible;
+            }
+            }
+
+        }
+
+        private void ButtonArrow_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Button arrow in arrowsButton)
+            {
+                arrow.Visibility = Visibility.Collapsed;
+            }
+            board.IsEnabledTrue();
+            string nameButton = (sender as Button).Name;
+            char direction = nameButton[12];
+            char NumberSquare = nameButton[11];
+            board.Rotation(direction, NumberSquare-49); //decrease number by 48 because of ASCI and decrese by 1 because of numeration from 0 in array
+            board.ShowWiner();
+        }
+    }
+}
